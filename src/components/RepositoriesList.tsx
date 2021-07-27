c64cc41d9d40afe4f8f5ffcc332a48f3b576a843
HassanKhan123 <hk2602809@gmail.com>
2021-07-27 23:01:42 +0500

    typed selectors created

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelectors';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
 
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    state => state.repositories,
+  );
+  console.log(data);
 
   const searchHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -17,7 +17,7 @@ export const searchRepositories = (term: string) => {
         },
       });
 
-      const names = data.objects.map((result: any) => {
+      const names = data.results.map((result: any) => {
         return result.package.name;
       });
 
